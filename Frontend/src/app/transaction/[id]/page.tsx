4796e20f65be888fd1dbee6758837ecc8326db6f
Tanvir Hasan Tamal <tanvirh103@gmail.com>
2025-06-17 04:31:59 +0600

    updated

--- Frontend/src/app/transaction/[id]/page.tsx
+++ Frontend/src/app/transaction/[id]/page.tsx
@@ -2,7 +2,6 @@ import Footer from "@/app/Component/footer";
 import Navbar from "@/app/Component/Navbar";
 import TransactionDetails from "@/app/Component/TransactionDetails";
 import axios from "axios";
-
 export default async function transactionDetails({ params }: { params: any }) {
   const id=await params.id
   const response = await axios.get(
